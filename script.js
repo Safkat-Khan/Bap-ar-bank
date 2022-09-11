4811afb06d4c52296c10409e1d64f6011377ab12
Safkat khan Tafsir <safkatkhan420@gmail.com>
2022-09-12 02:24:53 +0600

    fourth commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 
+document.getElementById('btn-submit').addEventListener('click',function(){
+    console.log('Submit Button clicked');
+   // alert('Submit Button clicked');
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    console.log(email);
 
+    const passField = document.getElementById('user-pass');
+    const pass = passField.value;
+    console.log(pass);
 
+})
