068207f29e4d0bd180fb3d36228e62c078404372
Safkat khan Tafsir <safkatkhan420@gmail.com>
2022-09-13 10:47:09 +0600

    Eight commit

--- deposit.js
+++ deposit.js
@@ -1,9 +1,25 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
 
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousdepositTotalString = depositTotalElement.innerText;
+    const previousdepositTotal = parseFloat(previousdepositTotalString);
 
+    const currentDepositTotal = previousdepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal ;
+
+    const totalBalance = document.getElementById('balance-total');
+    const totalBalanceString = totalBalance.innerText;
+    const covtTotalBalance = parseFloat(totalBalanceString);
+    
+    const finalBalance = newDepositAmount + covtTotalBalance;
+    totalBalance.innerText = finalBalance;
+
+
+
+
+
+    depositField.value ='';
 })
